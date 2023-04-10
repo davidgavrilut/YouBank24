0fdab72b2db4b8a8cc5ab42e526c6b70df6f9065
davidgavrilut1 <davidgavrilut1@gmail.com>
2023-04-10 13:16:05 +0300

    Add project files.

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace YouBank24.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
