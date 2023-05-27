a75862603b0436168d7a4517da73c74344ea9776
davidgavrilut1 <davidgavrilut1@gmail.com>
2023-05-27 17:30:18 +0300

    Transaction status and type fix

--- src/Controllers/SendMoneyController.cs
+++ src/Controllers/SendMoneyController.cs
@@ -28,19 +28,21 @@ public class SendMoneyController : Controller {
             _claimsIdentity = (ClaimsIdentity)User.Identity;
             _claim = _claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
             transaction.TransactionId = Guid.NewGuid().ToString();
+            transaction.TransactionType = StaticDetails.TransactionTypeInstantMoney;
             transaction.TransactionTimestamp = DateTime.Now;
-            transaction.TransactionStatus = StaticDetails.StatusPending;
             var receiverUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Email == email);
             transaction.ReceiverUserId = receiverUser.Id;
             transaction.AccountId = _unitOfWork.Account.GetFirstOrDefault(a => a.ApplicationUserId == _claim.Value).AccountId;
-            _unitOfWork.Transaction.Add(transaction);
             _unitOfWork.Account.Update(_claim.Value, -transaction.Amount);
             _unitOfWork.Account.Update(transaction.ReceiverUserId, transaction.Amount);
+            transaction.TransactionStatus = StaticDetails.StatusSuccess;
+            _unitOfWork.Transaction.Add(transaction);
             _unitOfWork.Save();
             TempData["success"] = "$" + transaction.Amount + " was successfully sent to " + receiverUser.FirstName + " " + receiverUser.LastName + ".";
             return RedirectToAction("Index", "Home");
-        } else
+        } else {
             return RedirectToAction("Index", "SendMoney");
+        }
     }
 
     #region API CALLS
@@ -49,11 +51,11 @@ public class SendMoneyController : Controller {
         _claimsIdentity = (ClaimsIdentity)User.Identity;
         _claim = _claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
         IEnumerable<ApplicationUser> users = _unitOfWork.ApplicationUser.GetAll(x => x.Id != _claim.Value);
-        List<object> usersBriefData = new List<object>();
+        List<object> sendMoneyData = new List<object>();
         foreach(var user in users) {
-            usersBriefData.Add( new { firstName = user.FirstName, lastName = user.LastName, email = user.Email });
+            sendMoneyData.Add( new { firstName = user.FirstName, lastName = user.LastName, email = user.Email });
         }
-        return Json(usersBriefData);
+        return Json(sendMoneyData);
     }
 
     public IActionResult GetUserBalance() {
--- src/Utils/StaticDetails.cs
+++ src/Utils/StaticDetails.cs
@@ -2,6 +2,8 @@
     public class StaticDetails {
         public const string StatusPending = "Pending";
         public const string StatusSuccess = "Success";
-        public const string StatusFailed = "Processing";
+        public const string StatusFailed = "Failed";
+
+        public const string TransactionTypeInstantMoney = "Instant transfer";
     }
 }
