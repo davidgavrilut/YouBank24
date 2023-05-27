12e4e27ca1e6dc072119750ba71da53d8c0e983b
davidgavrilut1 <davidgavrilut1@gmail.com>
2023-05-27 18:42:40 +0300

    Create TransactionList controller and view

---
+++ src/Controllers/TransactionListController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YouBank24.Controllers {
+    public class TransactionListController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
