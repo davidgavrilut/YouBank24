16c15b67c6e12ca2d2429dda89ce97f404b2c0ea
davidgavrilut <davidgavrilut1@gmail.com>
2023-09-27 21:57:00 +0300

    Send Email (controller + template)

--- src/Controllers/SendMoneyController.cs
+++ src/Controllers/SendMoneyController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using YouBank24.Models;
@@ -9,13 +10,15 @@ namespace YouBank24.Controllers;
 public class SendMoneyController : Controller {
     private readonly ILogger<HomeController> _logger;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IEmailSender _emailSender;
     private ClaimsIdentity _claimsIdentity;
     private Claim _claim;
 
 
-    public SendMoneyController(ILogger<HomeController> logger, IUnitOfWork unitOfWork) {
+    public SendMoneyController(ILogger<HomeController> logger, IUnitOfWork unitOfWork, IEmailSender emailSender) {
         _logger = logger;
         _unitOfWork = unitOfWork;
+        _emailSender = emailSender;
     }
 
     [Route("Send")]
@@ -35,14 +38,18 @@ public class SendMoneyController : Controller {
             transaction.TransactionId = Guid.NewGuid().ToString();
             transaction.TransactionType = StaticDetails.TransactionTypeInstantMoney;
             transaction.TransactionTimestamp = DateTime.Now;
-            var receiverUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Email == email);
+            var receiverUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(r => r.Email == email);
             transaction.ReceiverUserId = receiverUser.Id;
             transaction.AccountId = _unitOfWork.Account.GetFirstOrDefault(a => a.ApplicationUserId == _claim.Value).AccountId;
+            var senderUserId = _unitOfWork.Account.GetFirstOrDefault(si => si.AccountId == transaction.AccountId).ApplicationUserId;
+            var senderUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(s => s.Id == senderUserId);
             _unitOfWork.Account.Update(_claim.Value, -transaction.Amount);
             _unitOfWork.Account.Update(transaction.ReceiverUserId, transaction.Amount);
             transaction.TransactionStatus = StaticDetails.StatusSuccess;
             _unitOfWork.Transaction.Add(transaction);
             _unitOfWork.Save();
+            _emailSender.SendEmailAsync(senderUser.Email, EmailMessages.SenderMessageSubject, EmailMessages.SenderMessageBody(senderUser.FirstName, senderUser.LastName, receiverUser.FirstName, receiverUser.LastName, transaction.Amount, transaction.Note));
+            _emailSender.SendEmailAsync(receiverUser.Email, EmailMessages.ReceiverMessageSubject, EmailMessages.ReceiverMessageBody(senderUser.FirstName, senderUser.LastName, receiverUser.FirstName, receiverUser.LastName, transaction.Amount, transaction.Note));
             if (transaction.Note?.Length > 0) {
                 TempData["success"] = "$" + transaction.Amount + " was successfully sent to " + receiverUser.FirstName + " " + receiverUser.LastName + " with the note \"" + transaction.Note + "\"";
             } else {
---
+++ src/Utils/EmailMessages.cs
@@ -0,0 +1,33 @@
+﻿namespace YouBank24.Utils
+{
+    public class EmailMessages
+    {
+        public const string SenderMessageSubject = "New Transaction Sent";
+        public const string ReceiverMessageSubject = "New Transaction Received";
+
+        public static string SenderMessageBody(string senderFirstName, string senderLastName, string receiverFirstName, string receiverLastName, float amount, string note)
+        {
+            if (note.Length > 0)
+            {
+                return $"<h3>New Transaction Sent</h3><hr style=\"\r\n    width: 10%;\r\n    margin-left: 0;\r\n\"><p>Ad new transaction has been successfully created on the YouBank24 platform.</p><p>You, {senderFirstName} {senderLastName}, sent ${amount} to {receiverFirstName} {receiverLastName}.</p></p>You also included thte following note: \"{note}\"</p><hr style=\"\r\n    width: 10%;\r\n    margin-left: 0;\r\n\"><p style=\"font-style: italic; font-weight: 600\">YouBank Team</p>";
+            } else
+            {
+                return $"<h3>New Transaction Sent</h3><hr style=\"\r\n    width: 10%;\r\n    margin-left: 0;\r\n\"><p>A new transaction has been successfully created on the YouBank24 platform.</p><p>You, {senderFirstName} {senderLastName}, sent ${amount} to {receiverFirstName} {receiverLastName}.<p><hr style=\"\r\n    width: 10%;\r\n    margin-left: 0;\r\n\"><p style=\"font-style: italic; font-weight: 600\">YouBank Team</p>";
+            }
+        }
+
+        public static string ReceiverMessageBody(string senderFirstName, string senderLastName, string receiverFirstName, string receiverLastName, float amount, string note)
+        {
+            if (note.Length > 0) 
+            {
+                return $"<h3>New Transaction Received</h3><hr style=\"\r\n    width: 10%;\r\n    margin-left: 0;\r\n\"><p>A new transaction has been successfully created on the YouBank24 platform.</p><p>You, {receiverFirstName} {receiverLastName}, have just received ${amount} from {senderFirstName} {senderLastName}.</p><p>The transaction also came with the following note: \"{note}\"</p><hr style=\"\r\n    width: 10%;\r\n    margin-left: 0;\r\n\"><p style=\"font-style: italic; font-weight: 600\">YouBank Team</p>";
+            } 
+            else
+            {
+                return $"<h3>New Transaction Received</h3><hr style=\"\r\n    width: 10%;\r\n    margin-left: 0;\r\n\"><p>A new transaction has been successfully created on the YouBank24 platform.</p><p>You, {receiverFirstName} {receiverLastName}, have just received ${amount} from {senderFirstName} {senderLastName}.</p><hr style=\"\r\n    width: 10%;\r\n    margin-left: 0;\r\n\"><p style=\"font-style: italic; font-weight: 600\">YouBank Team</p>";
+            }
+            
+        }
+
+    }
+}
