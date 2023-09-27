7996658d282295603eb808e396ec826ed84daab8
davidgavrilut <davidgavrilut1@gmail.com>
2023-09-27 19:00:10 +0300

    Email config 2

--- src/Utils/EmailSender.cs
+++ src/Utils/EmailSender.cs
@@ -1,8 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 
 namespace YouBank24.Utils;
 public class EmailSender : IEmailSender {
     public Task SendEmailAsync(string email, string subject, string htmlMessage) {
+        var emailToSend = new MimeMessage();
+        emailToSend.From.Add(MailboxAddress.Parse("davidgavrilut1@gmail.com"));
+        emailToSend.To.Add(MailboxAddress.Parse(email));
+        emailToSend.Subject = subject;
+        emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+        {
+            Text = htmlMessage
+        };
+
+        // Sending Email
+        using (var emailClient = new SmtpClient())
+        {
+            emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+            emailClient.Authenticate("davidgavrilut1@gmail.com", "");
+            emailClient.Send(emailToSend);
+            emailClient.Disconnect(true);
+        }
         return Task.CompletedTask;
     }
 }
