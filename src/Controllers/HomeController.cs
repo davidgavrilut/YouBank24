10dbac269ebc09c757c715b472ad5e4f8b06bbc5
davidgavrilut <davidgavrilut1@gmail.com>
2023-11-22 16:54:58 +0200

    re-add error method HomeController

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -42,6 +42,13 @@ public class HomeController : Controller {
         }
     }
 
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+
     public IActionResult Privacy() {
         return View();
     }
